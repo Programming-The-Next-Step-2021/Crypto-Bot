dffe7cd5ffde569d61d1d4a35f4997fe9fcbea59
AlexisDaniels <alexis_daniels@hotmail.com>
2021-05-11 22:04:17 -0500

    Creating a package

---
+++ Crypto_bot_pkg/crypto_selection.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#This is where I'll code the functions
+
+def which_crypto():
+    print("This function will allow you to choose between BTC, ETH, ADA or DOGE coins")
+
