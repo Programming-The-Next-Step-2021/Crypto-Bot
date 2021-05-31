49bb6231f753b62c5ebc6640f2bb990fd6c24f11
DEBORA STEIN <deborastein@MacBook-Air-de-DEBORA.local>
2021-05-30 20:15:28 -0500

    updated with all imports

--- setup.py
+++ setup.py
@@ -6,7 +6,18 @@ setuptools.setup(
     version = 0.1, 
     author = "Alexis",
     author_email = "alexis_daniels@hotmail.com",
-    description = "A package where you can keep track of your favorite cryptocurrencies and obtain buy/sell suggestion based on sentiment analysis of influencers",
+    description = "A package where you can keep track of your favorite cryptocurrencies and obtain buy/sell suggestion based on the RSI",
     license = 'MIT',
-    packages = setuptools.find_packages()
+    packages = setuptools.find_packages(),
+    install_requires = [
+        'numpy',
+        'pandas',
+        'matplotlib',
+        'asyncio',
+        'websocket',
+        'websockets',
+        'asyncio',
+        'TA-Lib',
+        'tkinter',
+    ]
     )
